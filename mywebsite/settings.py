940c6549b36ad9e95058053f865fecfb5dcee00d
INSECT5386 <shinnara.yuchan@gmail.com>
2025-02-26 14:10:19 +0900

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -1,1 +1,17 @@
-
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR / 'templates',  # 템플릿 기본 경로 설정
+        ],
+        'APP_DIRS': True,  # 앱의 templates 폴더 자동 탐색
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
